a709a99cf336b2b9fa26b8aa8a6532156feac316
NikhilPeri <nikhil.peri@shopify.com>
2017-11-11 14:26:33 -0500

    shitty code

---
+++ app/device/accelerometer.rb
@@ -0,0 +1,24 @@
+class Device::Accelerometer
+  include Beaglebone
+
+  def read
+    puts read_i2c(6, 0x1D)
+  end
+
+  def write
+  end
+
+  private
+
+  def write_i2c(data)
+    i2c_gyro.write(0x53, data.pack('C*'))
+  end
+
+  def read_i2c(bytes, start_address)
+    i2c_accelerometeri2c_gyro.read(0x53, bytes, [start_address].pack('C*'))
+  end
+
+  def i2c
+    @i2c_gyro ||= I2CDevice.new(:I2C2)
+  end
+end
--- app/device/imu.rb
+++
@@ -1,9 +0,0 @@
-class Device::IMU
-  include Beaglebone
-
-  def read
-  end
-
-  def write
-  end
-end
